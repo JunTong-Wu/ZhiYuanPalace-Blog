9a1f2875dc33c1aedee772d7c818fae125cee26c
Nick <juntong.wu@travel-easy.com>
2023-12-19 21:48:35 +0800

    修改banner文字

--- src/design/components/page/ZyBanner/ZyBanner.vue
+++ src/design/components/page/ZyBanner/ZyBanner.vue
@@ -1,26 +1,18 @@
 <template>
-  <div bg="bg-2" h-40 w-full rounded-2xl overflow-hidden relative>
-    <img
-      src="@/src/assets/image/banner.png"
-      alt=""
-      w-full
-      h-full
-      object-cover
-    />
+  <div h-100 w-full relative>
     <div
-      absolute
-      inset-2
       flex
       flex-col
       items-center
       justify-center
-      bg="bg-5"
       rounded-xl
       backdrop-blur-xl
+      h-full
     >
+      <img src="@/src/assets/image/logo.png" alt="" w-20 />
       <h2 m-4 text-sm>欢迎访问第五代知鸢宫！</h2>
-      <p m-0 text-xs>知鸢宫正在开发中，请保持友链。</p>
-      <p m-0 text-xs>开发过程中可能随时无法访问，请谅解。</p>
+      <p m-0 text-xs>知鸢宫正在维护中，请保持友链。</p>
+      <p m-0 text-xs>维护过程中无法访问，请谅解。</p>
     </div>
   </div>
 </template>
--- src/design/components/page/ZyList/ZyList.vue
+++ src/design/components/page/ZyList/ZyList.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <ul>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
+      <li></li>
     </ul>
   </div>
 </template>
