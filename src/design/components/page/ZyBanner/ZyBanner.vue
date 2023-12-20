297c78dfcaa6493d1759f2a3128d47dcf9972a8a
Nick <juntong.wu@travel-easy.com>
2023-12-20 09:08:11 +0800

    去除banner模糊

--- src/design/components/page/ZyBanner/ZyBanner.vue
+++ src/design/components/page/ZyBanner/ZyBanner.vue
@@ -1,14 +1,6 @@
 <template>
   <div h-100 w-full relative>
-    <div
-      flex
-      flex-col
-      items-center
-      justify-center
-      rounded-xl
-      backdrop-blur-xl
-      h-full
-    >
+    <div flex flex-col items-center justify-center rounded-xl h-full>
       <img src="@/src/assets/image/logo.png" alt="" w-20 />
       <h2 m-4 text-sm>欢迎访问第五代知鸢宫！</h2>
       <p m-0 text-xs>知鸢宫正在维护中，请保持友链。</p>
