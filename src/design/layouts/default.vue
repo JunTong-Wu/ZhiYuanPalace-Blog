d691c8d6270ff40d8cc2d51ad71c447a33e3996f
Nick <juntong.wu@travel-easy.com>
2024-02-05 20:58:46 +0800

    去除意外的颜色过渡

--- src/design/layouts/default.vue
+++ src/design/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <Header pl-sidebar />
-  <main flex-1 pl-sidebar pt-header transition-all bg="bg-transparent">
+  <main flex-1 pl-sidebar pt-header transition-width bg="bg-transparent">
     <slot />
   </main>
   <Footer class="vertical-layout" />
