a26a3deb2b667f886f1dd02f8a8727c790dc6f4a
Nick <juntong.wu@travel-easy.com>
2024-02-15 15:38:05 +0800

    优化卡片动画的逻辑

--- components/ZyStoreStyleCard.vue
+++ components/ZyStoreStyleCard.vue
@@ -43,35 +43,30 @@ const openCard = (element: any) => {
   if (element) {
     // 获取元素的位置
     const rect = element.getBoundingClientRect();
-    // 输出元素的位置信息
-    // console.log("Top position:", rect.top);
-    // console.log("Right position:", rect.right);
-    // console.log("Bottom position:", rect.bottom);
-    // console.log("Left position:", rect.left);
-    // console.log("Width:", rect.width);
-    // console.log("Height:", rect.height);
-    // 改变卡片定位
-    element.style.position = "fixed";
-    element.style.top = rect.top + "px";
-    element.style.left = rect.left + "px";
-    element.style.width = rect.width + "px";
-    element.style.height = rect.height + "px";
-    element.style.zIndex = "20";
+    // 复制一个绝对定位的卡片，制作动画
+    const copyElement = element.cloneNode(true);
+    copyElement.style.position = "fixed";
+    copyElement.style.top = rect.top + "px";
+    copyElement.style.left = rect.left + "px";
+    copyElement.style.width = rect.width + "px";
+    copyElement.style.height = rect.height + "px";
+    copyElement.style.zIndex = "20";
+    document.body.appendChild(copyElement);
 
     setTimeout(() => {
-      element.classList.add("transition-in");
+      copyElement.classList.add("transition-in");
       setTimeout(() => {
-        element.style.top = 0 + "px";
-        element.style.left = mainViewLeft + "px";
-        element.style.width = windowWidth - mainViewLeft + "px";
-        element.style.height = windowHeight + "px";
+        copyElement.style.top = 0 + "px";
+        copyElement.style.left = mainViewLeft + "px";
+        copyElement.style.width = windowWidth - mainViewLeft + "px";
+        copyElement.style.height = windowHeight + "px";
         const header = document.querySelector("header");
         if (header) {
           header.classList.remove("transition-translate-in");
           header.classList.add("transition-translate-out");
         }
         setTimeout(() => {
-          let href = element.href;
+          let href = copyElement.href;
           let url = new URL(href);
           let path = url.pathname;
           router.replace(path);
@@ -79,6 +74,8 @@ const openCard = (element: any) => {
             header.classList.remove("transition-translate-out");
             header.classList.add("transition-translate-in");
           }
+          // 动画结束，删除临时卡片
+          document.body.removeChild(copyElement);
         }, 600);
       });
     });
