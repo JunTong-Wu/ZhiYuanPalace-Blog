c71110af1a2b5e58a819579dc885749a58d2ac6c
Nick <juntong.wu@travel-easy.com>
2024-02-15 16:00:55 +0800

    卡片动画添加预备效果

--- components/ZyStoreStyleCard.vue
+++ components/ZyStoreStyleCard.vue
@@ -41,44 +41,53 @@ const openCard = (element: any) => {
     mainViewLeft = view.left;
   }
   if (element) {
-    // 获取元素的位置
-    const rect = element.getBoundingClientRect();
-    // 复制一个绝对定位的卡片，制作动画
-    const copyElement = element.cloneNode(true);
-    copyElement.style.position = "fixed";
-    copyElement.style.top = rect.top + "px";
-    copyElement.style.left = rect.left + "px";
-    copyElement.style.width = rect.width + "px";
-    copyElement.style.height = rect.height + "px";
-    copyElement.style.zIndex = "20";
-    document.body.appendChild(copyElement);
+    // 预备动画
+    element.classList.add("transition-prepare");
 
+    // 正式动画
     setTimeout(() => {
-      copyElement.classList.add("transition-in");
+      element.classList.remove("transition-prepare");
       setTimeout(() => {
-        copyElement.style.top = 0 + "px";
-        copyElement.style.left = mainViewLeft + "px";
-        copyElement.style.width = windowWidth - mainViewLeft + "px";
-        copyElement.style.height = windowHeight + "px";
-        const header = document.querySelector("header");
-        if (header) {
-          header.classList.remove("transition-translate-in");
-          header.classList.add("transition-translate-out");
-        }
+        // 获取元素的位置
+        const rect = element.getBoundingClientRect();
+        // 复制一个绝对定位的卡片，制作动画
+        const copyElement = element.cloneNode(true);
+        copyElement.style.position = "fixed";
+        copyElement.style.top = rect.top + "px";
+        copyElement.style.left = rect.left + "px";
+        copyElement.style.width = rect.width + "px";
+        copyElement.style.height = rect.height + "px";
+        copyElement.style.zIndex = "20";
+        document.body.appendChild(copyElement);
+
         setTimeout(() => {
-          let href = copyElement.href;
-          let url = new URL(href);
-          let path = url.pathname;
-          router.replace(path);
-          if (header) {
-            header.classList.remove("transition-translate-out");
-            header.classList.add("transition-translate-in");
-          }
-          // 动画结束，删除临时卡片
-          document.body.removeChild(copyElement);
-        }, 600);
-      });
-    });
+          copyElement.classList.add("transition-in");
+          setTimeout(() => {
+            copyElement.style.top = 0 + "px";
+            copyElement.style.left = mainViewLeft + "px";
+            copyElement.style.width = windowWidth - mainViewLeft + "px";
+            copyElement.style.height = windowHeight + "px";
+            const header = document.querySelector("header");
+            if (header) {
+              header.classList.remove("transition-translate-in");
+              header.classList.add("transition-translate-out");
+            }
+            setTimeout(() => {
+              let href = copyElement.href;
+              let url = new URL(href);
+              let path = url.pathname;
+              router.replace(path);
+              if (header) {
+                header.classList.remove("transition-translate-out");
+                header.classList.add("transition-translate-in");
+              }
+              // 动画结束，删除临时卡片
+              document.body.removeChild(copyElement);
+            }, 600);
+          });
+        }, 20);
+      }, 100);
+    }, 100);
   }
 };
 </script>
