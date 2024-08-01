9adec568f95215ea47b6211c8332ffe23fd4a563
Nick <juntong.wu@travel-easy.com>
2024-08-01 21:57:29 +0800

    调整一些距离

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,17 @@
   <div>
     <div class="root pr-toolbar pt-main">
       <div class="p-4 xs:p-5 sm:p-6 md:p-7 lg:p-8">
-        <h1 class="text-4xl font-bold mb-4">Welcome to ZhiYuan Palace!</h1>
+        <div class="container flex py-8 portrait:flex-col">
+          <div class="w-1/2 p-8 flex flex-col justify-center portrait:order-1 portrait:w-full portrait:items-center">
+            <h2 class="text-5xl portrait:text-2xl font-bold portrait:font-normal mb-8">欢迎访问知鸢宫</h2>
+            <p class="text-lg text-text-2 portrait:text-base mb-4">『知鸢宫』是我的个人博客，用来存放原创文章、网络文案、照片等回忆。愿有岁月可回首，且以深情共白头。</p>
+            <p class="text-lg text-text-2 portrait:text-base mb-4">『知鸢宫』取自成语『知书达理，鸢飞鱼跃』。须是操存涵养，定云止水中，有鸢飞鱼跃的景象，风狂雨骤处，有波恬浪静的风光，才见处一化齐之妙。</p>
+          </div>
+          <div class="w-1/2 portrait:mx-auto">
+            <img src="@/assets/image/LooperGroup.png" alt="">
+          </div>
+        </div>
+
       </div>
       <Toolbar class="absolute top-0 bottom-0 right-0 w-toolbar pt-main" />
     </div>
