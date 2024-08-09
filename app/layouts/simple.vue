2ba1f3b4dc2d5966eb8473f5dcac04db2b461afe
Nick <juntong.wu@travel-easy.com>
2024-08-09 22:01:37 +0800

    预留登陆页面

--- app/layouts/simple.vue
+++ app/layouts/simple.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="layout-simple" class="relative overflow-hidden">
-    <slot />
+
+    <div
+        id="layout-simple-main"
+        class="layout-simple-main relative z-20"
+    >
+      <main>
+        <div class="main-view text-base relative overflow-hidden">
+          <div class="main-view-inner">
+            <slot />
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
-<script setup lang="ts">
-onMounted(() => {
-  // console.log('111111111111111')
-})
-</script>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    TEST
+    <div class="absolute top-0 left-0 z-10 w-full h-full flex items-center justify-center">
+      <div class="bg-bg-bestCard p-8 rounded-2xl min-w-[20rem] flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold text-center mb-4">登录知鸢宫</h1>
+
+        <div class="w-full flex flex-col items-center">
+          登录页暂未开发，敬请期待
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 <script setup lang="ts">
 useHead({
-  title: "视频",
+  title: "登录",
   meta: [{ name: "description", content: "知书达理，鸢飞鱼跃" }],
   bodyAttrs: {
-    class: "page-video",
+    class: "page-login",
   },
 });
 definePageMeta({
