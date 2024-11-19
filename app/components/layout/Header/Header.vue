5526ebf99fd97ff090130598e4c2c4e3307c725f
JunTong Wu <570446022@qq.com>
2024-11-19 23:00:22 +0800

    状态栏添加日期时间

--- app/components/layout/Header/Header.vue
+++ app/components/layout/Header/Header.vue
@@ -9,8 +9,18 @@
       <div class="bg-[rgba(127,127,127,0.2)]  w-96 flex items-center px-4 rounded-full h-[70%]">
         <span class="text-xs text-text-1">暂无歌词，享受好音乐</span>
       </div>
-      <!-- 按键区 -->
+      <!-- 时间区 -->
+      <div class="absolute left-[50%] -translate-x-[50%] text-text-2">
+        <ZyButton class="flex items-center justify-center p-1 px-2" title="时钟" type="icon">
+          {{ time }}
+        </ZyButton>
+      </div>
       <div class="absolute right-0 top-0 h-full flex items-center">
+        <!-- 日期区 -->
+        <div class="flex items-center justify-end h-full text-text-2 p-2">
+          {{ todayDate }}
+        </div>
+        <!-- 按键区 -->
         <div class="flex justify-end h-full items-center gap-1">
           <!-- 多语言抽屉 -->
           <ZyPopover title="切换语言" background="var(--bg-level-1)">
@@ -150,6 +160,40 @@ const openMoreDrawer = () => {
 const closeMoreDrawer = () => {
   moreDisplay.value = false;
 };
+
+// 时钟
+const time = ref("");
+const updateTime = () => {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  time.value = `${hours}:${minutes}:${seconds}`;
+};
+// 初始化时间
+updateTime();
+// 每秒钟更新一次时间
+let timeInterval: NodeJS.Timeout; // 定义一个变量来存储定时器的返回值
+onMounted(() => {
+  timeInterval = setInterval(updateTime, 1); // 将定时器的返回值存储在 timeInterval 变量中
+});
+onBeforeUnmount(() => {
+  clearInterval(timeInterval); // 在组件卸载前清除定时器
+});
+
+//日期
+const todayDate = ref("");
+const updateDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const week = date.getDay();
+  const weekStr = ["日", "一", "二", "三", "四", "五", "六"];
+  todayDate.value = `${year}-${month}-${day} 星期${weekStr[week]}`;
+};
+// 初始化时间
+updateDate();
 </script>
 <style>
 ::view-transition-new(root),
